d453b1845bcebe216838ba5fa4b1cdaa58d668e2
PHILIPPE Romain <romain.philippe78@free.fr>
2015-11-22 17:51:19 +0100

    Changement de la page Map => creation d'un fragment de map et on affiche la position

--- app/src/main/java/com/example/user/localizone/MapActivity.java
+++ app/src/main/java/com/example/user/localizone/MapActivity.java
@@ -1,46 +1,84 @@
 package com.example.user.localizone;
 
-import android.support.v4.app.FragmentActivity;
+/**
+ * Created by Romain on 21/11/2015.
+ */
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+
+public class MapActivity extends AppCompatActivity {
+
+    private TextView locationText;
+    private TextView addressText;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        locationText = (TextView) findViewById(R.id.location);
+        addressText = (TextView) findViewById(R.id.address);
+
+        //replace GOOGLE MAP fragment in this Activity
+        replaceMapFragment();
     }
 
+    private void replaceMapFragment() {
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        // Enable Zoom
+        map.getUiSettings().setZoomGesturesEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng paris = new LatLng(48.8534100, 2.3488000);
-        mMap.addMarker(new MarkerOptions().position(paris).title("Marker in Paris"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        //set Map TYPE
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //enable Current location Button
+        map.setMyLocationEnabled(true);
+
+        //set "listener" for changing my location
+        map.setOnMyLocationChangeListener(myLocationChangeListener());
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng(-33.87365, 151.20689))
+                .radius(10000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
     }
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener() {
+        return new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+
+                //Marker marker;
+                //marker = map.addMarker(new MarkerOptions().position(loc));
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+                locationText.setText("You are at [" + longitude + " ; " + latitude + " ]");
+
+                //get current address by invoke an AsyncTask object
+                new GetAdressTask(MapActivity.this).execute(String.valueOf(latitude), String.valueOf(longitude));
+            }
+        };
+    }
+
+    public void callBackDataFromAsyncTask(String address) {
+        addressText.setText(address);
+    }
+
 }
