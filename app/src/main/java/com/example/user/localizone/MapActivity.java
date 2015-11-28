5d866ee668a998db8f81aa7d47e68111e708deed
PHILIPPE Romain <romain.philippe78@free.fr>
2015-11-28 21:35:16 +0100

    affichage des zone sur la carte

--- app/src/main/java/com/example/user/localizone/MapActivity.java
+++ app/src/main/java/com/example/user/localizone/MapActivity.java
@@ -8,9 +8,12 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.FloatMath;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,14 +22,21 @@ import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class MapActivity extends AppCompatActivity {
 
     private TextView locationText;
     private TextView addressText;
+    private TextView checkzone;
     private GoogleMap map;
     ListView listView;
+    JSONArray the_json_array_area=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +45,11 @@ public class MapActivity extends AppCompatActivity {
 
         locationText = (TextView) findViewById(R.id.location);
         addressText = (TextView) findViewById(R.id.address);
+        checkzone = (TextView) findViewById(R.id.checkzone);
 
         //replace GOOGLE MAP fragment in this Activity
         replaceMapFragment();
-
+       // displayAreaMap();
         listView = (ListView) findViewById(R.id.list);
         String[] alertes = new String[]{"Alerte 1", "Alerte 2", "Alerte 3", "Alerte 4"};
 
@@ -56,23 +67,14 @@ public class MapActivity extends AppCompatActivity {
     private void replaceMapFragment() {
         map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
                 .getMap();
-
         // Enable Zoom
         map.getUiSettings().setZoomGesturesEnabled(true);
-
         //set Map TYPE
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
         //enable Current location Button
         map.setMyLocationEnabled(true);
-
         //set "listener" for changing my location
         map.setOnMyLocationChangeListener(myLocationChangeListener());
-        Circle circle = map.addCircle(new CircleOptions()
-                .center(new LatLng(-33.87365, 151.20689))
-                .radius(10000)
-                .strokeColor(Color.RED)
-                .fillColor(Color.BLUE));
     }
 
     private GoogleMap.OnMyLocationChangeListener myLocationChangeListener() {
@@ -80,16 +82,23 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void onMyLocationChange(Location location) {
                 LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-                double longitude = location.getLongitude();
-                double latitude = location.getLatitude();
+                double longitudeCurrent = location.getLongitude();
+                double latitudeCurrent = location.getLatitude();
 
                 //Marker marker;
                 //marker = map.addMarker(new MarkerOptions().position(loc));
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
-                locationText.setText("You are at [" + longitude + " ; " + latitude + " ]");
+                //map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+                locationText.setText("You are at [" + longitudeCurrent + " ; " + latitudeCurrent + " ]");
+
+                ////////////////////////////
+                // functioncheck in zone
+                if(the_json_array_area!=null){
+                    checkZone(latitudeCurrent,longitudeCurrent);
+
+                }
 
                 //get current address by invoke an AsyncTask object
-                new GetAdressTask(MapActivity.this).execute(String.valueOf(latitude), String.valueOf(longitude));
+                new GetAdressTask(MapActivity.this).execute(String.valueOf(latitudeCurrent), String.valueOf(longitudeCurrent));
             }
         };
     }
@@ -97,5 +106,84 @@ public class MapActivity extends AppCompatActivity {
     public void callBackDataFromAsyncTask(String address) {
         addressText.setText(address);
     }
+
+    public void displayAreaMap(){
+        StringBuilder area =HttpRequest.sendRequest(getApplicationContext(), "getArea/HJBUIB688G8G8");
+        JSONObject jsonObject=null;
+
+        try {
+            jsonObject = new JSONObject(area.toString());
+            the_json_array_area = jsonObject.getJSONArray("area");
+            int size = the_json_array_area.length();
+            ArrayList<JSONObject> arrays = new ArrayList<JSONObject>();
+            for (int i = 0; i < size; i++) {
+                JSONObject another_json_object = the_json_array_area.getJSONObject(i);
+                //Blah blah blah...
+                arrays.add(another_json_object);
+                Double latitude= another_json_object.getDouble("latitude");
+                Double longitude= another_json_object.getDouble("longitude");
+                Integer distance= another_json_object.getInt("distance");
+
+                Log.d("Myapp", distance.toString());
+
+                Circle circle = map.addCircle(new CircleOptions()
+                        .center(new LatLng(latitude,longitude))
+                        .radius(distance)
+                        .strokeColor(Color.RED)
+                        .fillColor(0x40ff0000));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        Toast.makeText(getApplicationContext(), area.toString(), Toast.LENGTH_LONG).show();
+    }
+
+
+    public void checkZone(double latitudeCurrent, double longitudeCurrent){
+        Boolean checkinzone=false;
+        int size_array =the_json_array_area.length();
+        if(size_array>0){
+            Log.d("TEST IF", " ok ko ok ok");
+            ArrayList<JSONObject> arrays = new ArrayList<JSONObject>();
+            for (int i = 0; i < size_array; i++) {
+                Log.d(" i ; ",String.valueOf(i));
+                JSONObject another_json_object = null;
+                try {
+                    another_json_object = the_json_array_area.getJSONObject(i);
+                    //Blah blah blah...
+                    arrays.add(another_json_object);
+                    double latitude2= another_json_object.getDouble("latitude");
+                    double longitude2= another_json_object.getDouble("longitude");
+                    Integer distance2= another_json_object.getInt("distance");
+
+                    double distancecalcule=Distance.distance(latitude2, longitude2, latitudeCurrent, longitudeCurrent, "metre");
+
+                    Log.d(" latitude2 ; ",String.valueOf(latitude2));
+                    Log.d(" longitude2 ; ",String.valueOf(longitude2));
+                    Log.d(" distance2 ; ",String.valueOf(distance2));
+
+                    Log.d(" distancecalcule ; ",String.valueOf(distancecalcule));
+                    if(distancecalcule<distance2){
+                        checkinzone=true;
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            if(checkinzone){
+                Log.d(" functioncheck" , "dans la zone");
+                checkzone.setText("You are good");
+            }else{
+                Log.d(" functioncheck" , " pas  dans la zone");
+                checkzone.setText("You are not good mec ! Bouge de la");
+            }
+        }
+    }
+
 
 }
