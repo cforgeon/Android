c23d3204ae7066413a7ad7be255f18a89459a988
amandine <amande.bourget@gmail.com>
2015-12-06 03:32:59 +0100

    inscription avec verification des champs

--- app/src/main/java/com/example/user/localizone/Inscription.java
+++ app/src/main/java/com/example/user/localizone/Inscription.java
@@ -3,13 +3,25 @@ package com.example.user.localizone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Inscription extends AppCompatActivity {
+    private EditText mail;
+    private EditText confMail;
+    private EditText password;
+    private EditText confPassword;
+    private TextView textError;
+    private boolean boolMail;
+    private boolean boolPass;
+
+    public Inscription(){
+        boolMail = false;
+        boolPass = false;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,14 +40,45 @@ public class Inscription extends AppCompatActivity {
 
     }
 
+    public void inscription() {
+        confMail = (EditText) findViewById(R.id.inscrConfMail);
+        confPassword = (EditText) findViewById(R.id.inscrConfpassword);
+        mail = (EditText) findViewById(R.id.inscrMail);
+        password = (EditText) findViewById(R.id.inscrPassword);
+        textError = (TextView) findViewById(R.id.textErrorConf);
 
-    public void inscription(){
-        EditText inscrMail = (EditText) findViewById(R.id.inscrMail);
-        EditText inscrPassword = (EditText) findViewById(R.id.inscrPassword);
-        String res = (HttpRequest.sendRequest(getApplicationContext(), "createUser/" + inscrMail.getText().toString() + "/" + inscrPassword.getText().toString()).toString());
-        if(!res.isEmpty()){
-            Intent intent = new Intent(Inscription.this, Inscription.class);
-            startActivity(intent);
+        if(confMail.getText().toString().equals(mail.getText().toString()) && confMail.getText().toString()!="" && confMail.getText().toString()!=" ") {
+            boolMail = true;
+
+            if(confPassword.getText().toString().equals(password.getText().toString()) && confPassword.getText().toString()!="" && confPassword.getText().toString()!=" ") {
+                boolPass = true;
+                System.out.println(confPassword.getText().toString() + password.getText().toString());
+            }
+            else{
+                textError.setText("Error password confirmation");
+            }
+        }
+        else{
+            textError.setText("Error mail confirmation");
+        }
+
+        if(boolMail == true && boolPass == true ){
+            String res = (HttpRequest.sendRequest(getApplicationContext(), "createUser/" + mail.getText().toString() + "/" + password.getText().toString()).toString());
+            boolMail = false;
+            boolPass = false;
+            if(res.equals("adresse email already exist")){
+                textError.setText("Error adresse email already exist");
+            }
+            else{
+                if (!res.isEmpty()) {
+                    Intent intent = new Intent(Inscription.this, Inscription.class);
+                    startActivity(intent);
+                }
+            }
         }
     }
 }
+
+
+
+
